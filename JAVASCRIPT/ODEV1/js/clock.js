089ece494a2a17628a8a430e95fb389bd61ea8ae
derya ünver <dryunver94@gmail.com>
2021-08-17 23:50:03 +0300

    javascript ödev1 yapıldı

---
+++ JAVASCRIPT/ODEV1/js/clock.js
@@ -0,0 +1,23 @@
+let myName=prompt("Adınız Nedir?")
+let info =document.querySelector("#myName")
+info.innerHTML=`${info.innerHTML}<strong> ${myName}</strong>`
+
+let date = new Date()
+let weekday = new Array(7);
+weekday[0] = "Pazar";
+weekday[1] = "Pazartesi";
+weekday[2] = "Salı";
+weekday[3] = "Çarşamba";
+weekday[4] = "Perşembe";
+weekday[5] = "Cuma";
+weekday[6] = "Cumartesi";
+let n = weekday[date.getDay()];
+
+let hours = date.getHours()
+let minute=date.getMinutes()
+let seconds= date.getSeconds()
+let day=date.getDay()
+
+document.querySelector("#myClock").innerHTML =` <div> ${hours}:${minute}:${seconds} ${n}</div>` 
+
+
