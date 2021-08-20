71d452d39bc01ca12cd2bc7fd0877c0bdf1471a1
derya ünver <dryunver94@gmail.com>
2021-08-20 16:13:48 +0300

    js-ödev1 yapıldı

--- JAVASCRIPT/ODEV1/js/clock.js
+++ JAVASCRIPT/ODEV1/js/clock.js
@@ -10,6 +10,19 @@ weekday[2] = "Salı";
 weekday[3] = "Çarşamba";
 weekday[4] = "Perşembe";
 weekday[5] = "Cuma";
+
+let myName=prompt("Adınız Nedir?")
+let info =document.querySelector("#myName")
+info.innerHTML=`${info.innerHTML}<strong> ${myName}</strong>`
+
+let date = new Date()
+let weekday = new Array(7);
+weekday[0] = "Pazar";
+weekday[1] = "Pazartesi ";
+weekday[2] = "Salı";
+weekday[3] = "Çarşamba";
+weekday[4] = "Perşembe";
+weekday[5] = "Cuma";
 weekday[6] = "Cumartesi";
 let n = weekday[date.getDay()];
 
